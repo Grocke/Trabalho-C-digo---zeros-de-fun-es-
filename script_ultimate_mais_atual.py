3dea0470120a69c9cfba386a97cdc145baad4e74
Gustavo Roque da Silva <61297395+Grocke@users.noreply.github.com>
2020-09-18 23:08:07 -0300

    AGORA VAI #FUNCIONA 100%

--- script_ultimate_mais_atual.py
+++ script_ultimate_mais_atual.py
@@ -30,7 +30,7 @@ def bisseccao(a, b, e, f):
 
     else:
         return "Não há raiz neste intervalo"
-print(bisseccao(2,3,0.01,"x**2 - 6"))
+# print(bisseccao(2,3,0.01,"x**2 - 6"))
 
 #Derivada
 def derivada(f, x, dx = 1e-6):
@@ -38,31 +38,6 @@ def derivada(f, x, dx = 1e-6):
     return df/(2*dx)
 
 # Metodo de Newton
-def newton(f, x, tol = 0.001, maxIter = 5):
-
-    x = x0
-    fx = f(x)
-    
-    for _ in range(maxIter):
-        
-        if abs(fx) < tol:
-            break
-
-        fpx = derivada(f, x)
-        if abs (fpx) < tol:
-            break
-
-        x = x - fx/fpx
-        fx = f(x)
-        
-    return x
-
-func = lambda x: x**2 - x - 1
-x0 = 2
-x = newton(func, x0, tol = 0.001, maxIter = 5)
-print("Solution: x = {}, f(x) = {}".format(x, func(x)))
-
-
 
 root = Tk()
 root.title("Calculadora C.N")
@@ -90,8 +65,6 @@ def botao_newton():
     interentryNewton.grid()
     interentry1Newton.grid()
     resultadoNewton.grid()
-    
-
 
 def botao_biseccao():
     metodo2.grid_remove()
@@ -116,27 +89,54 @@ def botao_biseccao():
     calculate.grid()
     resultado.grid()
     
-
+    
 def calculate_botao():
      
         funcao = fxentry.get()
-        eps = int(epsentry.get())
-        in1 = int(interentry.get())
+        eps = float(epsentry.get())
+        in1 = float(interentry.get())
         in2 = float(interentry1.get())
         result = bisseccao(eps,in1,in2,funcao)
         resultado.delete(0,'end')
         resultado.insert(0,result)
+        return
 
-def calculate_botao_newton():
+def calculate_botao_newton():        
+        funcao = fxentryNewton.get()
+        epsl = float(epsentryNewton.get())
+        x0 = float(interentryNewton.get())
+        in3 = int(interentry1Newton.get())
+        #func = lambda x: x**2 - x 
+        # x = newton(func, x0, tol = 0.001, maxIter = 10)
 
-        funcao = fxentry.get()
-        eps = int(epsentryNewton.get())
-        x0 = int(interentryNewton.get())
-        in2 = float(interentry1Newton.get())
-        result = newton(funcao,x0,tol = 0.001, maxIter = 5)
+        # Metodo de Newton
+        def newton(f, x, tol = 0.001, maxIter = 5):
+
+            x = x0
+            fx = f(x)
+
+            for _ in range(maxIter):
+
+                if abs(fx) < tol:
+                    break
+
+                fpx = derivada(f, x)
+                if abs (fpx) < tol:
+                    break
+
+                x = x - fx/fpx
+                fx = f(x)
+
+            return x
+
+        func = lambda x: eval(funcao)
+        #x0 = 1.5
+        x = newton(func, x0, epsl, in3)
+        print("Solution: x = {}, f(x) = {}".format(x, func(x)))
+
+        #result = newton(func, 6, tol = 0.001, maxIter = 10)
         resultadoNewton.delete(0,'end')
-        resultadoNewton.insert(0,result)    
-
+        resultadoNewton.insert(0,x)
 
 
 valor1 = Button(root,text="Método de Newton",width=35,borderwidth=5, command=botao_newton)
@@ -156,7 +156,7 @@ metodo3 = Label(root, text = 'Biseccao', font=('calibre',10, 'bold'))
 metodo3.grid(row=2,column=1,padx=10,pady=10,sticky=W)
 metodo2.grid_remove()
 
-fx1 = Label(root, text = 'f(x)', font=('calibre',10, 'bold')) 
+fx1 = Label(root, text = 'f(x):', font=('calibre',10, 'bold')) 
 fx1.grid(row=3,column=0,padx=10,pady=10,sticky=E)
 
 fxentry = Entry(root,width=35,borderwidth=5)
@@ -164,12 +164,12 @@ fxentry.grid(row=3,column=1)
 
 fxentryNewton = Entry(root,width=35,borderwidth=5)
 fxentryNewton.grid(row=3,column=1)
-fxentryNewton.grid_remove()
+# fxentryNewton.grid_remove()
 
-eps = Label(root, text = 'Epsolon', font=('calibre',10, 'bold')) 
+eps = Label(root, text = 'Até:', font=('calibre',10, 'bold')) 
 eps.grid(row=4,column=0,padx=10,pady=10,sticky=E)
 
-B = Label(root, text = 'B', font=('calibre',10, 'bold')) 
+B = Label(root, text = 'Epsolon:', font=('calibre',10, 'bold')) 
 B.grid(row=4,column=0,padx=10,pady=10,sticky=E)
 B.grid_remove()
 
@@ -183,7 +183,7 @@ epsentryNewton.grid_remove()
 inter = Label(root, text = 'De:', font=('calibre',10, 'bold')) 
 inter.grid(row=5,column=0,padx=10,pady=10,sticky=E)
 
-x0 = Label(root, text = 'x0', font=('calibre',10, 'bold')) 
+x0 = Label(root, text = 'x0:', font=('calibre',10, 'bold')) 
 x0.grid(row=5,column=0,padx=10,pady=10,sticky=E)
 x0.grid_remove()
 
@@ -194,7 +194,7 @@ interentryNewton = Entry(root,width=35,borderwidth=5)
 interentryNewton.grid(row=5,column=1)
 interentryNewton.grid_remove()
 
-inter1 = Label(root, text = 'Até:', font=('calibre',10, 'bold')) 
+inter1 = Label(root, text = 'Epsolon:', font=('calibre',10, 'bold')) 
 inter1.grid(row=6,column=0,padx=10,pady=10,sticky=E)
 
 interacao = Label(root, text = 'Interação:', font=('calibre',10, 'bold')) 
@@ -225,6 +225,6 @@ resultado.grid(row=8,column=1,columnspan=1,padx=10,pady=10)
 
 resultadoNewton = Entry(root,width=35,borderwidth=5)
 resultadoNewton.grid(row=8,column=1,columnspan=1,padx=10,pady=10)
-resultadoNewton.grid_remove()
+# resultadoNewton.grid_remove()
 
 root.mainloop()
